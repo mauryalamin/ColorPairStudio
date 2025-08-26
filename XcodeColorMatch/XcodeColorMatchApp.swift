c6e6cf7b08d1cfb9559d1ae689ee1c4a25a7f444
Maury Alamin <malamin@comcast.net>
2025-08-26 15:56:27 -0500

    Initial Commit

---
+++ XcodeColorMatch/XcodeColorMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeColorMatchApp.swift
+//  XcodeColorMatch
+//
+//  Created by Maury Alamin on 8/26/25.
+//
+
+import SwiftUI
+
+@main
+struct XcodeColorMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ XcodeColorMatchTests/XcodeColorMatchTests.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeColorMatchTests.swift
+//  XcodeColorMatchTests
+//
+//  Created by Maury Alamin on 8/26/25.
+//
+
+import Testing
+@testable import XcodeColorMatch
+
+struct XcodeColorMatchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
